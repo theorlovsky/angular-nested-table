9402f9834448b1913d91645748f606aa894af80c
Alex Orlovsky <alex94orlovsky@gmail.com>
2020-07-07 16:39:24 +0300

    change printWidth from 80 to 120

--- src/app/mock-data/mock-data.service.ts
+++ src/app/mock-data/mock-data.service.ts
@@ -19,28 +19,24 @@ export class MockDataService {
   private generateData(): TableData {
     const platforms = this.generatePlatforms();
 
-    return new Array(this.targetDataLength)
-      .fill(undefined)
-      .map<TableDataItem>(() => {
-        const platformIndex = random.number(platforms.length - 1);
-        const leads = random.number(50);
-        const revenue = leads ? +finance.amount(0, 500, 2) : 0;
+    return new Array(this.targetDataLength).fill(undefined).map<TableDataItem>(() => {
+      const platformIndex = random.number(platforms.length - 1);
+      const leads = random.number(50);
+      const revenue = leads ? +finance.amount(0, 500, 2) : 0;
 
-        return {
-          id: random.uuid(),
-          platform: platforms[platformIndex],
-          company: company.companyName(),
-          leads,
-          revenue,
-          rpl: revenue ? revenue / leads : 0,
-        };
-      });
+      return {
+        id: random.uuid(),
+        platform: platforms[platformIndex],
+        company: company.companyName(),
+        leads,
+        revenue,
+        rpl: revenue ? revenue / leads : 0,
+      };
+    });
   }
 
   private generatePlatforms(): Array<TableDataItem['platform']> {
-    return new Array(this.targetPlatformsLength)
-      .fill(undefined)
-      .map(() => this.capitalize(internet.domainWord()));
+    return new Array(this.targetPlatformsLength).fill(undefined).map(() => this.capitalize(internet.domainWord()));
   }
 
   private capitalize(str: string): string {
